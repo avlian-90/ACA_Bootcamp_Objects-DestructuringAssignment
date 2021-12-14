2de49fcf875ab92d341bbb57e8124ed1f53009fd
Liana <lianavagyan.90@gmail.com>
2021-12-15 03:14:33 +0400

    feat: task was done and added

---
+++ spread1.js
@@ -0,0 +1,12 @@
+// How to take arguments in sum function?
+
+function sum(...args) {
+  return args.reduce((sum, current) => {
+    return sum + current;
+  });
+}
+
+console.log(sum(1, 2)); // prints 3
+console.log(sum(1, 2, 3)); // prints 6
+
+// spreading arguments (...args) and giving it to the function as a parameter
