ae34b1e60b7a520f27c754ef6ea1fee4ed5d441f
Liana <lianavagyan.90@gmail.com>
2021-12-15 02:59:08 +0400

    feat: task was done and added

---
+++ destructuring.js
@@ -0,0 +1,7 @@
+// Get name, country and jod using destructuring
+
+let person = { name: "Sarah", country: "Armenia", job: "Developer" };
+let { name, country, job } = person;
+console.log(name); // "Sarah"
+console.log(country); // "Armenia"
+console.log(job); // "Developer"
