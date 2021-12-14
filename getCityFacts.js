535d0a5cdcb1896e176ed6fd643412386a71d8d9
Liana <lianavagyan.90@gmail.com>
2021-12-15 03:59:59 +0400

    fix: fixed bugs and did changes

--- getCityFacts.js
+++ getCityFacts.js
@@ -8,10 +8,21 @@ continent */
 
 function getCityFacts(obj) {
   let { name, population, continent } = obj;
-  let facts = name + population + continent;
+  let facts = `${name} has a population of ${population} and is situated in ${continent}`;
   return facts;
 }
 
 console.log(
-  getCityFacts({ name: "Armenia", population: 3000000, continent: "Asia" })
-);
+  getCityFacts({
+    name: "Paris",
+    population: "2,140,526",
+    continent: "Europe",
+  })
+); // ➞ "Paris has a population of 2,140,526 and is situated in Europe"
+console.log(
+  getCityFacts({
+    name: "Tokyo",
+    population: "13,929,286",
+    continent: "Asia",
+  })
+); // ➞ "Tokyo has a population of 13,929,286 and is situated in Asia"
