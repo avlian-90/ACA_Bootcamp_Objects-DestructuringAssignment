07846c31e6bd3f8da0bffecc6a9d0caf781d4a22
Liana <lianavagyan.90@gmail.com>
2021-12-15 04:26:19 +0400

    feat: task was done and added

---
+++ printSum.js
@@ -0,0 +1,30 @@
+/* Create a function that takes infinite count of elements, operator and prints their sum. If there's no
+operator, then default should be + */
+
+function printSum(operator, ...args) {
+  let sum = args[0];
+  for (let i = 1; i < args.length; i++) {
+    switch (operator) {
+      case "*":
+        sum *= args[i];
+        break;
+      case "-":
+        sum -= args[i];
+        break;
+      case "/":
+        sum /= args[i];
+        break;
+      case "**":
+        sum **= args[i];
+        break;
+      default:
+        sum += args[i];
+    }
+  }
+  return sum;
+}
+
+console.log(printSum("*", 1, 2, 3)); // 6
+console.log(printSum(1, 2, 3, 4, 5)); // 15
+console.log(printSum("-", 1, 2, 3, 6, 7)); // -17
+console.log(printSum("**", 2, 3, 2)); // 64
